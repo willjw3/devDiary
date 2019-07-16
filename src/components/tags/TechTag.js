18d376ddaf8794fe371d9f4eec8184ab9e0d1ece
Will Ward <will.j.ward3@gmail.com>
2019-07-16 11:40:04 +0900

    Updated tech tags to use svg elements

--- src/components/tags/TechTag.js
+++ src/components/tags/TechTag.js
@@ -11,12 +11,29 @@ const TechTag = (props) => {
     const icon = /^Fa/.test(str) ? React.createElement(FontAwesome[name]) : React.createElement(Devicons[name]);
 
     return (
+        // <div className="d-inline-block p-1">
+        //     <Link to={`/tags/${tag}/`}>
+        //         <button
+        //             className="tech-tag text-white">
+        //             <p className="d-inline">{tech} </p>
+        //             <div className="d-inline" style={{ fontSize: size, color: color }}>{icon}</div>
+        //         </button>
+        //     </Link>
+
+        // </div>
+
         <div className="d-inline-block p-1">
             <Link to={`/tags/${tag}/`}>
                 <button
                     className="tech-tag text-white">
                     <p className="d-inline">{tech} </p>
-                    <div className="d-inline" style={{ fontSize: size, color: color }}>{icon}</div>
+                    <div className="d-inline" style={{ fontSize: size, color: color }}>
+                        <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style={{width: `${size}`, height: `${size}`, fill: `${color}`}}>
+                            <title>{tech}</title>
+                            <path
+                                d={name} />
+                        </svg>
+                    </div>
                 </button>
             </Link>
 
