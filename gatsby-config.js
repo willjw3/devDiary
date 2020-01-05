8dfc2e4efd6da52bd9c88bb58330a0f231eea679
willjw3 <will.j.ward3@gmail.com>
2020-01-05 18:51:59 +0900

    Stale commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,6 +51,15 @@ module.exports = {
             }
           }, 
           `gatsby-remark-responsive-iframe`,
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 290,
+            },
+          },
         ],
       },
     },
--- src/components/tags/TechTag.js
+++ src/components/tags/TechTag.js
@@ -18,7 +18,7 @@ const TechTag = (props) => {
                     className="tech-tag text-white">
                     <p className="d-inline">{tech} </p>
                     <div className="d-inline" style={{ fontSize: size, color: color }}>
-                        <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style={{width: `${size}`, height: `${size}`, fill: `${color}`}}>
+                        <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width={size} height={size} style={{fill: `${color}`}}>
                             <title>{tech}</title>
                             <path
                                 d={name} />
